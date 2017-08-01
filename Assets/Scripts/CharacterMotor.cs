99e6894ec9259770e66736d8c41c419430f871e4
Unknown <bm1952@nyu.edu>
2017-08-01 16:29:04 -0400

    Added Pistol script

--- Assets/Scripts/CharacterMotor.cs
+++ Assets/Scripts/CharacterMotor.cs
@@ -7,15 +7,9 @@ public class CharacterMotor : MonoBehaviour {
 	/* CONTROLLING */
 	public float moveSpeed = 200f;
 	public float jumpForce = 375f;
-	[HideInInspector] public Vector3 moveDir;
 
 	/* OBJECTS AND WEAPONS */
 	public bool isHoldingObject;
-	public GameObject barrelEnd; // <-- SET IN EDITOR
-	public GameObject bulletPrefab; // <-- SET IN EDITOR
-	private float bulletSpeed = 30f;
-	private float avgShotDistance = 20f;
-	private bool canAttack = true;
 
 	/* INTERNALS */
 	[HideInInspector] public float distToGround;
@@ -44,43 +38,9 @@ public class CharacterMotor : MonoBehaviour {
 
 	/* ACTION FUNCITONS */
 
-	public void Move() {
-		Vector3 yVelFix = new Vector3( 0, rb.velocity.y, 0 );
-		rb.velocity = moveDir * moveSpeed * Time.deltaTime;
-		rb.velocity += yVelFix;	//allows player to be affected by gravity
-	}
-
-	public void Jump() {
-		rb.AddForce( Vector3.up * jumpForce, ForceMode.Impulse );
-	}
-
 	public void Attack() {
-		if ( isHoldingObject && canAttack ) {
-			// Raycasts through center of the screen, gets point that overlaps the crosshair: 
-			Ray ray = Camera.main.ViewportPointToRay( new Vector3( 0.5f, 0.5f, Camera.main.nearClipPlane ));
-			RaycastHit hit;
-			Vector3 lookPoint; //used to track where the bullet should be fired towards
-
-			if ( Physics.Raycast( ray, out hit )) {
-				lookPoint = hit.point;
-			} else {
-				lookPoint = ray.GetPoint( avgShotDistance );
-			}
-
-			// Spawns bullet:
-			GameObject bulletInstance = Instantiate(
-				bulletPrefab,
-				barrelEnd.transform.position,
-				Quaternion.identity
-			) as GameObject;
-			bulletInstance.transform.SetParent( GameObject.Find( "*DYNAMIC*" ).transform );
-
-			// Fires bullet toward the point:
-			bulletInstance.transform.LookAt( lookPoint );
-			bulletInstance.GetComponent<Rigidbody>().AddForce( bulletInstance.transform.forward * bulletSpeed, ForceMode.VelocityChange );
-
-			StartCoroutine( AttackWait( 0.6f ));
-			
+		if ( isHoldingObject ) {
+			GetComponentInChildren<Pistol>().Shoot();
 		}
 
 
@@ -98,12 +58,6 @@ public class CharacterMotor : MonoBehaviour {
 		 * 		Punch();
 		 * 	}
 		 */
-	}
-
-	public IEnumerator AttackWait( float seconds ) {
-		canAttack = false;
-		yield return new WaitForSeconds( seconds );
-		canAttack = true;
 	}
 
 	public void Grab() {
---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour {
+
+	public GameObject barrelEnd; // <-- SET IN EDITOR
+	public GameObject bulletPrefab; // <-- SET IN EDITOR
+
+	private float bulletSpeed = 30f;
+	private float avgShotDistance = 20f;
+	private bool canAttack = true;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	public void Shoot() {
+		// Raycasts through center of the screen, gets point that overlaps the crosshair: 
+		Ray ray = Camera.main.ViewportPointToRay( new Vector3( 0.5f, 0.5f, Camera.main.nearClipPlane ));
+		RaycastHit hit;
+		Vector3 lookPoint; //used to track where the bullet should be fired towards
+
+		if ( Physics.Raycast( ray, out hit )) {
+			lookPoint = hit.point;
+		} else {
+			lookPoint = ray.GetPoint( avgShotDistance );
+		}
+
+		// Spawns bullet:
+		GameObject bulletInstance = Instantiate(
+			bulletPrefab,
+			barrelEnd.transform.position,
+			Quaternion.identity
+		) as GameObject;
+		bulletInstance.transform.SetParent( GameObject.Find( "*DYNAMIC*" ).transform );
+
+		// Fires bullet toward the point:
+		bulletInstance.transform.LookAt( lookPoint );
+		bulletInstance.GetComponent<Rigidbody>().AddForce( bulletInstance.transform.forward * bulletSpeed, ForceMode.VelocityChange );
+
+		StartCoroutine( AttackWait( 0.6f ));
+	}
+
+	public IEnumerator AttackWait( float seconds ) {
+		canAttack = false;
+		yield return new WaitForSeconds( seconds );
+		canAttack = true;
+	}
+}
