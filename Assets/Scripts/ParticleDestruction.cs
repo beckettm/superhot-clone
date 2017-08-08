f10d56b833aada3ab0b204ff90c43c29a1ba9ab2
Unknown <dt1305@nyu.edu>
2017-08-08 15:05:26 -0400

    Buncha stuff.
    Crosshair image can change and proper mesh colliders are put in.

---
+++ Assets/Scripts/ParticleDestruction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!GetComponent<ParticleSystem>().IsAlive()){
+			Destroy (gameObject);
+		}
+	}
+}
